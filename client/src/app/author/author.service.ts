8cc112fbe5e26f3a475405ead337a85f7977b454
Sandra13988 <sandra13988@gmail.com>
2025-03-03 10:42:46 +0100

    First commit

---
+++ client/src/app/author/author.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs'; //El of es para usar los datos mockeados
+import { Pageable } from '../core/model/page/Pageable';
+import { Author } from './model/Author';
+import { AuthorPage } from './model/AuthorPage';
+import { AUTHOR_DATA } from './model/mock-authors'; //Esto era para usar los datos mockeados
+import { HttpClient } from '@angular/common/http';
+import { AUTHOR_DATA_LIST } from './model/mock-author-list'; //Esto era para usar los datos mockeados';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthorService {
+    constructor(private http: HttpClient) {}
+
+    private baseUrl = 'http://localhost:8080/author';
+
+    getAuthors(pageable: Pageable): Observable<AuthorPage> {
+        return this.http.post<AuthorPage>(this.baseUrl, { pageable: pageable });
+    }
+
+    saveAuthor(author: Author): Observable<Author> {
+        const { id } = author;
+        const url = id ? `${this.baseUrl}/${id}` : this.baseUrl;
+        return this.http.put<Author>(url, author);
+    }
+
+    deleteAuthor(idAuthor: number): Observable<void> {
+        return this.http.delete<void>(`${this.baseUrl}/${idAuthor}`);
+    }
+
+    
+
+    getAllAuthors(): Observable<Author[]> {
+      return this.http.get<Author[]>(this.baseUrl);
+  }
+}
